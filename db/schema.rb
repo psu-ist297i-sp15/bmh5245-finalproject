f06e6be686278eae6c49f1a0b0991da473d11177
Dan Coughlin <dan.coughlin@gmail.com>
2015-05-05 10:30:25 -0400

    had to make these changes to get it running

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424174836) do
+ActiveRecord::Schema.define(version: 20150505141626) do
 
   create_table "breeds", force: true do |t|
     t.string   "name"
